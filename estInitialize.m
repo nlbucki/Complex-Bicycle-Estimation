793f6d399e044d04b86cc84f085ea1cbb5ed0d1c
nlbucki <nlbucki@gmail.com>
2018-05-01 18:54:14 -0700

    Added comments

--- estInitialize.m
+++ estInitialize.m
@@ -1,31 +1,27 @@
 function internalState = estInitialize
-% Fill in whatever initialization you'd like here. This function
-% generates the internal state of the estimator at time 0. You may do
-% whatever you like here, but you must return something that is in the
-% format as may be used by your run() function.
-%
 
-% we make the interal state a structure, with the first three elements the
-% positions x, y; the angle theta; and our favourite colour.
-
-% note that there is *absolutely no prescribed format* for this internal state.
-% You can put in it whatever you like. Probably, you'll want to keep the position
-% and angle, and probably you'll remove the color.
-
+% Number of particles
 N = 500;
+
+% Initialize particles
 particles = zeros(5,N);
 for i=1:N
-    particles(1,i) = unifrnd(-20,20);
-    particles(2,i) = unifrnd(-20,20);
-    particles(3,i) = unifrnd(-pi,pi);
+    % Start roughly at (0,0)
+    particles(1,i) = unifrnd(-10,10);
+    particles(2,i) = unifrnd(-10,10);
+    
+    % Start facing roughly northeast
+    particles(3,i) = unifrnd(-pi/4, 3*pi/4);
+    
+    % Radius is +/- 5 percent of nominal value
     particles(4,i) = unifrnd(0.425*0.95,0.425*1.05);
+    
+    % Baseline is +/- 10 percent of nominal value
     particles(5,i) = unifrnd(0.8*0.9,0.8*1.1);
 end
 
-internalState.x = 0;
-internalState.y = 0;
-internalState.theta = pi/4;
 internalState.particles = particles; 
+
 end
 
 
--- estRun.m
+++ estRun.m
@@ -18,51 +18,72 @@ function [x,y,theta,internalStateOut] = estRun(~, dt, internalStateIn, steeringA
 %  est_theta: your current best estimate for the bicycle's rotation theta
 %  internalState: the estimator's internal state, in a format that can be understood by the next call to this function
 
-internalStateOut = internalStateIn;
+% Read in variables
 particles = internalStateIn.particles;
 omega = pedalSpeed;
 gamma = steeringAngle;
+
+% Variance of measurement noise as measured from calibration data file
 R = [1.0881, 1.5315;
      1.5315, 2.9845];
 
+% Propagate particles through the dynamics
 p_particles = zeros(size(particles));
 for i=1:size(particles,2)
+    % Create process noise for each particle
     v1 = normrnd(0,0.1);
     v2 = normrnd(0,0.1);
-    v3 = normrnd(0,0.05);
-    p_particles(:,i) = particles(:,i) + ...
-                     [5*particles(4,i)*(omega)*cos(particles(3,i));
-                     5*particles(4,i)*(omega)*sin(particles(3,i));
-                     5*particles(4,i)*(omega)*tan(gamma)/particles(5,i);
-                     0; 0]*dt + [v1;v2;v3;0;0];
+    
+    % First derivative of state vector
+    particle_dot = [5*particles(4,i)*(omega + v1)*cos(particles(3,i));
+                     5*particles(4,i)*(omega + v1)*sin(particles(3,i));
+                     5*particles(4,i)*(omega + v1)*tan(gamma + v2)/particles(5,i);
+                     0;
+                     0];
+    % Second derivative of state vector
+    particle_ddot = [-(5*particles(4,i)*(omega + v1))^2*sin(particles(3,i))*tan(gamma + v2)/particles(5,i);
+                     (5*particles(4,i)*(omega + v1))^2*cos(particles(3,i))*tan(gamma + v2)/particles(5,i);
+                     0;
+                     0;
+                     0];
+    % Second order dynamics approximation
+    p_particles(:,i) = particles(:,i) + dt*particle_dot + dt^2/2*particle_ddot;
 end
 
 if ~isnan(measurement(1)) & ~isnan(measurement(2))
+    % We have recieved a new measurement
     
+    % Generate estimates of measurements we expect to recieve
     m_particles = zeros(2,size(p_particles,2));
     for i=1:size(p_particles,2)
         m_particles(:,i) = [p_particles(1,i) + 0.5*p_particles(5,i)*cos(p_particles(3,i)), ...
                             p_particles(2,i) + 0.5*p_particles(5,i)*sin(p_particles(3,i))];
     end
+    
+    % Check probability of getting each estimated measurment given the
+    % actual measurement
     probs = mvnpdf(m_particles', measurement, R);
     if sum(probs) < 1e-9
-        % We messed up and got infeasible values. Reset to measurment
-        % values.
-        disp('Reset')
+        % Our estimator is too far from the measurement! Reset our
+        % estimate to match the latest measurement
+        disp('Estimator Reset!')
         for i=1:size(p_particles,2)
             p_particles(1:2,i) = [measurement(1) - 0.5*p_particles(5,i)*cos(p_particles(3,i));
                                   measurement(2) - 0.5*p_particles(5,i)*sin(p_particles(3,i))];
         end
         f_particles = p_particles;
-        %     Roughening (seems unnecessary)
+        
+        % Roughen particles after reset to prevent particle convergence
         sigma = zeros(size(f_particles,1),1);
         K = 0.01;
         for i=1:size(f_particles,1)
+           % sigma_i = K*E_i*N^(-1/d)
            sigma(i) = K*abs(peak2peak(f_particles(i,:)))*size(f_particles,2)^(-1/size(f_particles,1));
         end
         delta = mvnrnd(zeros(5,1), diag(sigma), size(f_particles,2));
         f_particles = f_particles + delta';
     else
+        % Estimate is reasonably valid, choose new particles
         alpha = 1/sum(probs);
         probs = alpha*probs;
         cumprobs = cumsum(probs);
@@ -74,25 +95,20 @@ if ~isnan(measurement(1)) & ~isnan(measurement(2))
         end
     end
 else
+    % We did not recieve a new measurement, so simply return the particles
+    % from the prior update
     f_particles = p_particles;
 end
-
-% figure(99);
-% plot(f_particles(1,:),f_particles(2,:), 'bx')
-% xlim([-50 50])
-% ylim([-50 50])
-% drawnow
 
 %% OUTPUTS %%
 % Update the internal state (will be passed as an argument to the function
 % at next run), must obviously be compatible with the format of
 % internalStateIn:
 
-internalStateOut.x = median(f_particles(1,:));
-internalStateOut.y = median(f_particles(2,:));
-internalStateOut.theta = median(f_particles(3,:));
+% Use the median particle as estimate to reduce effect of outliers
 internalStateOut.particles = f_particles;
-x = internalStateOut.x;
-y = internalStateOut.y;
-theta = internalStateOut.theta;
+x = median(f_particles(1,:));
+y = median(f_particles(2,:));
+theta = median(f_particles(3,:));
+
 end
